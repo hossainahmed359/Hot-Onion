3aaf2f11a1f3acacd6b16443aef16b3282536a9e
hossainahmed359 <hossainahmed359@gmail.com>
2021-10-16 12:43:03 +0600

    food section added

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import LoadFoodTypeByName from '../LoadFoodTypeByName/LoadFoodTypeByName';
 
 const Food = ({ foodsName }) => {
@@ -14,8 +15,14 @@ const Food = ({ foodsName }) => {
 
     return (
         <div>
-            <h1>{foods.length}</h1>
-            {foods.map(food => foods.indexOf(food) < 6 && <LoadFoodTypeByName key={food.idMeal} food={food}></LoadFoodTypeByName>)}
+            <Container className="mb-5">
+                <Row xs={1} md={2} lg={3} className="g-4">
+                    {foods.map(food => foods.indexOf(food) < 6 && <LoadFoodTypeByName key={food.idMeal} food={food}></LoadFoodTypeByName>)}
+                </Row>
+                <div className="text-center my-5">
+                    <button className="btn btn-secondary btn-large rounded">Checkout Our Food</button>
+                </div>
+            </Container>
         </div>
     );
 };
--- src/components/LoadFoodTypeByName/LoadFoodTypeByName.js
+++ src/components/LoadFoodTypeByName/LoadFoodTypeByName.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+import "./LoadFoodByName.css"
 
 const LoadFoodTypeByName = ({ food }) => {
-    console.log(food)
+    const { strMeal, strMealThumb, strInstructions } = food;
     return (
         <div>
-            <h1>Hello</h1>
+            <Col>
+                <Card className="food-card align-items-center text-center border-0">
+                    <Card.Img variant="top" className="w-50 rounded-circle my-3 shadow" src={strMealThumb} />
+                    <Card.Body>
+                        <Card.Title>{strMeal}</Card.Title>
+                        <Card.Text>
+                            {strInstructions.slice(0, 150)}...
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
         </div>
     );
 };
